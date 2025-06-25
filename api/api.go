4f932f821389ecc742d91598bda42dddac3567e1
Marcin Tojek <marcin@coder.com>
2025-06-25 17:20:05 +0200

    API

--- api/api.go
+++ api/api.go
@@ -1,39 +1,175 @@
 package api
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
 	"net/http"
+	"slices"
+	"strconv"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/mtojek/spiroflex-vent-clear"
+	"github.com/mtojek/spiroflex-vent-clear/econet"
 )
 
-func Create() http.Handler {
+type WebServer struct {
+	c *spiroflex.Config
+}
+
+type response struct {
+	Ok    bool   `json:"ok"`
+	Error string `json:"error,omitempty"`
+}
+
+func NewWebServer(c *spiroflex.Config) *WebServer {
+	return &WebServer{
+		c: c,
+	}
+}
+
+func (ws *WebServer) Handler() http.Handler {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 
 	r.Route("/api", func(r chi.Router) {
 		r.Route("/vent", func(r chi.Router) {
-			r.Get("/level/{level}", ventLevel)
-			r.Get("/pause", ventPause)
-			r.Get("/mode/{mode}", ventMode)
-			r.Get("/power/{state}", ventPower)
+			r.Get("/level/{level:[1-3]?}", ws.ventLevel)
+			r.Get("/pause", ws.ventPause)
+			r.Get("/mode/{mode:schedule|manual}", ws.ventMode)
+			r.Get("/power/{state:on|off}", ws.ventPower)
 		})
 	})
 	return r
 }
 
-func ventLevel(w http.ResponseWriter, r *http.Request) {
-	panic("not implemented yet")
+func (ws *WebServer) ventLevel(w http.ResponseWriter, r *http.Request) {
+	session, targetComponentID, err := ws.prepareEconet(r.Context())
+	if err != nil {
+		writeError(w, err)
+		return
+	}
+
+	levelStr := chi.URLParam(r, "level")
+	level, err := strconv.Atoi(levelStr)
+	if err != nil {
+		writeError(w, err)
+		return
+	}
+	level += 2 // levels are enumerated from 3 to 5
+
+	err = session.VentLevel(r.Context(), targetComponentID, fmt.Sprintf("%d", level))
+	if err != nil {
+		writeError(w, fmt.Errorf("unable to modify parameters: %w", err))
+		return
+	}
+	writeSuccess(w)
 }
 
-func ventPause(w http.ResponseWriter, r *http.Request) {
-	panic("not implemented yet")
+func (ws *WebServer) ventPause(w http.ResponseWriter, r *http.Request) {
+	session, targetComponentID, err := ws.prepareEconet(r.Context())
+	if err != nil {
+		writeError(w, err)
+		return
+	}
+
+	err = session.VentPause(r.Context(), targetComponentID)
+	if err != nil {
+		writeError(w, fmt.Errorf("unable to modify parameters: %w", err))
+		return
+	}
+	writeSuccess(w)
 }
 
-func ventMode(w http.ResponseWriter, r *http.Request) {
-	panic("not implemented yet")
+func (ws *WebServer) ventMode(w http.ResponseWriter, r *http.Request) {
+	session, targetComponentID, err := ws.prepareEconet(r.Context())
+	if err != nil {
+		writeError(w, err)
+		return
+	}
+
+	mode := chi.URLParam(r, "mode")
+	if mode == "schedule" {
+		mode = econet.PARAM_SCHEDULE_SCHEDULE
+	} else {
+		mode = econet.PARAM_SCHEDULE_MANUAL
+	}
+	err = session.VentMode(r.Context(), targetComponentID, mode)
+	if err != nil {
+		writeError(w, fmt.Errorf("unable to modify parameters: %w", err))
+		return
+	}
+	writeSuccess(w)
 }
 
-func ventPower(w http.ResponseWriter, r *http.Request) {
-	panic("not implemented yet")
+func (ws *WebServer) ventPower(w http.ResponseWriter, r *http.Request) {
+	session, targetComponentID, err := ws.prepareEconet(r.Context())
+	if err != nil {
+		writeError(w, err)
+		return
+	}
+
+	state := chi.URLParam(r, "state")
+	if state == "on" {
+		state = econet.PARAM_POWER_ON
+	} else {
+		state = econet.PARAM_POWER_OFF
+	}
+	err = session.VentPower(r.Context(), targetComponentID, state)
+	if err != nil {
+		writeError(w, fmt.Errorf("unable to modify parameters: %w", err))
+		return
+	}
+	writeSuccess(w)
+}
+
+func (ws *WebServer) prepareEconet(ctx context.Context) (*econet.MQTTSession, string, error) {
+	client, err := econet.New(ctx, ws.c)
+	if err != nil {
+		return nil, "", fmt.Errorf("unable to create client: %w", err)
+	}
+
+	installations, err := client.Installations(ctx)
+	if err != nil {
+		return nil, "", fmt.Errorf("unable to fetch installations: %w", err)
+	}
+
+	i := slices.IndexFunc(installations, func(ins econet.Installation) bool {
+		return ins.Name == ws.c.Installation.Name
+	})
+	if i < 0 {
+		return nil, "", fmt.Errorf("installation not found or invalid name: %w", err)
+	}
+	installationID := installations[i].ID
+
+	session, err := client.MQTT(ctx, installationID)
+	if err != nil {
+		return nil, "", fmt.Errorf("MQTT error: %w", err)
+	}
+
+	gcob, err := session.GetComponentsOnBus(ctx)
+	if err != nil {
+		return nil, "", fmt.Errorf("unable to fetch components on bus: %w", err)
+	}
+
+	var targetComponentID string
+	for _, c := range gcob {
+		if c.ComponentName == "ecoVENT MINI OEM" {
+			targetComponentID = c.ComponentID
+		}
+	}
+	return session, targetComponentID, nil
+}
+
+func writeError(w http.ResponseWriter, err error) {
+	resp := response{Error: err.Error()}
+	w.WriteHeader(http.StatusInternalServerError)
+	json.NewEncoder(w).Encode(resp)
+}
+
+func writeSuccess(w http.ResponseWriter) {
+	resp := response{Ok: true}
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(resp)
 }
--- cmd/ventclear/main.go
+++ cmd/ventclear/main.go
@@ -1,79 +1,27 @@
 package main
 
 import (
-	"context"
 	"log"
 	"net/http"
-	"slices"
 
 	"github.com/mtojek/spiroflex-vent-clear"
 	"github.com/mtojek/spiroflex-vent-clear/api"
-	"github.com/mtojek/spiroflex-vent-clear/econet"
 )
 
-func main2() {
+func main() {
 	c, err := spiroflex.LoadConfig()
 	if err != nil {
 		log.Fatalf("can't load config: %v", err)
 	}
 
+	webServer := api.NewWebServer(c)
 	srv := &http.Server{
 		Addr:    c.API.Endpoint,
-		Handler: api.Create(),
+		Handler: webServer.Handler(),
 	}
 
 	log.Printf("Server started at %v", c.API.Endpoint)
 	if err := srv.ListenAndServe(); err != http.ErrServerClosed {
 		log.Fatalf("srv.ListenAndServe failed: %v", err)
-	}
-}
-
-func main() {
-	ctx := context.Background()
-
-	c, err := spiroflex.LoadConfig()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	client, err := econet.New(ctx, c)
-	if err != nil {
-		log.Fatalf("unable to create client: %v", err)
-	}
-
-	installations, err := client.Installations(ctx)
-	if err != nil {
-		log.Fatalf("API call failed: %v", err)
-	}
-
-	i := slices.IndexFunc(installations, func(ins econet.Installation) bool {
-		return ins.Name == c.Installation.Name
-	})
-	if i < 0 {
-		log.Fatal("Installation not found or invalid name")
-	}
-
-	session, err := client.MQTT(ctx, installations[i].ID)
-	if err != nil {
-		log.Fatalf("MQTT error: %v", err)
-	}
-	defer session.Disconnect()
-
-	gcob, err := session.GetComponentsOnBus(ctx)
-	if err != nil {
-		log.Fatalf("GetComponentsOnBus failed: %v", err)
-	}
-
-	var targetComponentID string
-	for _, c := range gcob {
-		if c.ComponentName == "ecoVENT MINI OEM" {
-			targetComponentID = c.ComponentID
-		}
-	}
-	log.Printf("Target component: %s", targetComponentID)
-
-	err = session.VentPower(ctx, targetComponentID, econet.PARAM_POWER_OFF)
-	if err != nil {
-		log.Fatalf("PARAMS_MODIFICATION error: %v", err)
 	}
 }
